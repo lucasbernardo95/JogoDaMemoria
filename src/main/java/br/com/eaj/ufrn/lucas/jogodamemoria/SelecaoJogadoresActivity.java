f620c279bfd1a226292bcea6ed615b8cae4bc160
lucasbernardo95 <lucasbernardo95@gmail.com>
2016-08-23 18:01:52 -0300

    Inicio da construção dos layouts.

---
+++ src/main/java/br/com/eaj/ufrn/lucas/jogodamemoria/SelecaoJogadoresActivity.java
@@ -0,0 +1,13 @@
+package br.com.eaj.ufrn.lucas.jogodamemoria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelecaoJogadoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_jogadores);
+    }
+}
---
+++ src/main/java/br/com/eaj/ufrn/lucas/jogodamemoria/fimDeJogoActivity.java
@@ -0,0 +1,13 @@
+package br.com.eaj.ufrn.lucas.jogodamemoria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fimDeJogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fim_de_jogo);
+    }
+}
